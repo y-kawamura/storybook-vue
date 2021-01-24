d6788d7076910c82be32c5631f854e9ee1e4109a
y-kawamura <tetora.y@gmail.com>
2021-01-24 14:07:02 +0900

    fix(Task): disable input checkbox event

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="list-item" :class="task.state">
     <label class="checkbox">
-      <input type="checkbox" :checked="isChecked" />
-      <span @click="$emit('archive-task', task.id)"></span>
+      <input type="checkbox" :checked="isChecked" disabled name="checked" />
+      <span
+        class="checkbox-custom"
+        @click="$emit('archive-task', task.id)"
+      ></span>
     </label>
     <div class="title">
-      <input type="text" readonly :value="task.title" />
+      <input
+        type="text"
+        readonly
+        :value="task.title"
+        placeholder="Input title"
+      />
     </div>
     <div class="actions">
       <a v-if="!isChecked" @click="$emit('pin-task', task.id)">
