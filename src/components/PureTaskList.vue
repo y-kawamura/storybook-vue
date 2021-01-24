181b338bbc3941b97672be81a1faa0424b64bd1b
y-kawamura <tetora.y@gmail.com>
2021-01-24 13:08:59 +0900

    feat(TaskList): task state uses Vuex

---
+++ src/components/PureTaskList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="list-items">
+    <template v-if="loading">
+      <div v-for="n in 6" :key="n" class="loading-item">
+        <span class="glow-checkbox"></span>
+        <span class="glow-text">
+          <span>Loading</span><span>cool</span><span>state</span>
+        </span>
+      </div>
+    </template>
+    <template v-else-if="isEmpty" class="list-items">
+      <div class="wrapper-message">
+        <span class="icon-check"></span>
+        <div class="title-message">You have no tasks</div>
+        <div class="subtitle-message">Sit back and relax</div>
+      </div>
+    </template>
+    <template v-else>
+      <Task
+        v-for="task in taskInOrder"
+        :key="task.id"
+        :task="task"
+        v-on="$listeners"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+
+export default {
+  name: "PureTaskList",
+
+  components: {
+    Task,
+  },
+
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+      default: () => {},
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    taskInOrder() {
+      return [
+        ...this.tasks.filter((task) => task.state === "TASK_PINNED"),
+        ...this.tasks.filter((task) => task.state !== "TASK_PINNED"),
+      ];
+    },
+    isEmpty() {
+      return this.tasks.length === 0;
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,63 +1,29 @@
 <template>
-  <div class="list-items">
-    <template v-if="loading">
-      <div v-for="n in 6" :key="n" class="loading-item">
-        <span class="glow-checkbox"></span>
-        <span class="glow-text">
-          <span>Loading</span><span>cool</span><span>state</span>
-        </span>
-      </div>
-    </template>
-    <template v-else-if="isEmpty" class="list-items">
-      <div class="wrapper-message">
-        <span class="icon-check"></span>
-        <div class="title-message">You have no tasks</div>
-        <div class="subtitle-message">Sit back and relax</div>
-      </div>
-    </template>
-    <template v-else>
-      <Task
-        v-for="task in taskInOrder"
-        :key="task.id"
-        :task="task"
-        v-on="$listeners"
-      />
-    </template>
-  </div>
+  <PureTaskList
+    :tasks="tasks"
+    v-on="$listeners"
+    @archive-task="archiveTask"
+    @pin-task="pinTask"
+  />
 </template>
 
 <script>
-import Task from "./Task";
+import PureTaskList from "./PureTaskList";
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "TaskList",
 
   components: {
-    Task,
-  },
-
-  props: {
-    tasks: {
-      type: Array,
-      required: true,
-      default: () => {},
-    },
-    loading: {
-      type: Boolean,
-      default: false,
-    },
+    PureTaskList,
   },
 
   computed: {
-    taskInOrder() {
-      return [
-        ...this.tasks.filter((task) => task.state === "TASK_PINNED"),
-        ...this.tasks.filter((task) => task.state !== "TASK_PINNED"),
-      ];
-    },
-    isEmpty() {
-      return this.tasks.length === 0;
-    },
+    ...mapState(["tasks"]),
+  },
+
+  methods: {
+    ...mapActions(["archiveTask", "pinTask"]),
   },
 };
 </script>
